ec610eb8a5a50077f1fab07acfc27a69b827902d
deepak Sharma <ds555299@gmail.com>
2025-12-22 15:56:54 +0530

    Data Transformation done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,1 +1,89 @@
-   
+import sys
+from dataclasses import dataclass
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+from src.utlis import save_object
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts', "preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self):                                              #This function is responsible for data transformation
+        try:
+            numerical_columns=["reading score", "writing score"]
+            categorical_columns=[
+                "gender",
+                "race/ethnicity",
+                "parental level of education",
+                "lunch",
+                "test preparation course",
+            ]
+
+            num_pipeline=Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),    #missing_values
+                    ("scaler", StandardScaler())     
+                ]
+            )
+
+            cat_pipeline=Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    ("one_hot_encoder", OneHotEncoder()), 
+                    ("scaler", StandardScaler())     
+                ]   
+            )
+            
+            logging.info(f"Numerical columns: {numerical_columns}")
+            logging.info(f"Categorical columns: {categorical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                    ("num_pipeline",num_pipeline,numerical_columns),
+                    ("cat_pipeline", cat_pipeline,categorical_columns)
+                ]
+            )
+
+            return preprocessor
+       
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+    def initiate_data_transformation(self,train_path,test_path):
+
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+            logging.info("Obtaining preprocessing object")
+
+            preprocessing_obj=self.get_data_transformer_object()
+            target_column_name="math score"
+            numerical_columns=["reading score", "writing score"]
+            input_feature_train_df=train_df.drop(columns=[target_column_name], axis=1)
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
+    
